d405c64f29c8807b60d0c4299b49a826b53f05bf
Guillaume Nodet <gnodet@gmail.com>
2024-09-30 12:47:38 +0200

    Code style

--- plugin-maven/src/main/java/com/tisonkun/os/maven/DetectPropertyContributor.java
+++ plugin-maven/src/main/java/com/tisonkun/os/maven/DetectPropertyContributor.java
@@ -47,7 +47,8 @@ public class DetectPropertyContributor implements PropertyContributor {
 
     @Override
     public void contribute(Map<String, String> map) {
-        logger.info("The os-detector Maven 4 extension is registered, OS and CPU architecture properties will be provided.");
+        logger.info(
+                "The os-detector Maven 4 extension is registered, OS and CPU architecture properties will be provided.");
         DetectExtension.disable();
 
         final Properties props = new Properties();
